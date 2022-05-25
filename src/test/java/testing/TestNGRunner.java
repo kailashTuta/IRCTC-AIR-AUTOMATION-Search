8292082971e70426e3115f61a94eeeb77b7e487a
kailashtuta <kailashtuta2000@gmail.com>
2022-05-25 10:21:52 +0530

    Generated TestNG and Cucumber Reports

--- src/test/java/testing/TestNGRunner.java
+++ src/test/java/testing/TestNGRunner.java
@@ -4,7 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 public class TestNGRunner {
-	@CucumberOptions(features = "src/test/resources/features", glue = "stepDefnitions")
+	@CucumberOptions(
+			features = "src/test/resources/features", 
+			glue = "stepDefnitions",
+			plugin = { "pretty", "html:target/cucumber-reports.html" },
+			monochrome = true
+			)
 	public class RunTestNGTest extends AbstractTestNGCucumberTests {
 
 	}
